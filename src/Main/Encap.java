a174e36ce1f563e8be7cde82c21b46dd1053bf27
Vishnu <rachapudivishnu3@gmail.com>
2025-03-14 19:49:56 +0530

    added interfaces,Collections

--- src/Main/Encap.java
+++ src/Main/Encap.java
@@ -1,5 +1,11 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Iterator;
+
 public class Encap{
 	private String fname;
 
@@ -10,11 +16,58 @@ public class Encap{
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
+	
+	//-------------------------------------------------------------
+	interface myinterface{
+		public void myInterfaceMethod();
+	}
+	
+	class hero implements myinterface{
+
+		@Override
+		public void myInterfaceMethod() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	
+	enum Level {
+		  LOW,
+		  MEDIUM,
+		  HIGH
+		}
+	//------------------------------------------------------------------
 	public static void main(String[] args) {
 		Encap newObject = new Encap();
 		newObject.setFname("Vishnu");
 		System.out.println (newObject.getFname());
+		
+		ArrayList<String> arrList = new ArrayList<String>();
+		arrList.add("Vishnu");
+		arrList.add("Akshaya");
+		
+		Collections.sort(arrList);
+		arrList.size();
+		System.out.println(arrList.get(0));
+		
+		LinkedList <String> linkedList = new LinkedList<String>();
+		linkedList.add("HI");
+		linkedList.addFirst("Lol");
+		
+		System.out.println(linkedList);
+	Level myVar = Level.MEDIUM;
 	
+	
+	 try {
+	      int[] myNumbers = {1, 2, 3};
+	      System.out.println(myNumbers[10]);
+	    } catch (Exception e) {
+	      System.out.println("Something went wrong.");
+	    } finally {
+	      System.out.println("The 'try catch' is finished.");
+	    }
 }
 
 
